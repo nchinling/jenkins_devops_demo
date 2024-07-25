e2ae5f74ed60bbe9d18b7f887e69fa84f77be985
nchinling <nchinling@gmail.com>
2024-07-25 14:40:42 +0800

    new print message

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! I am learning Jenkins." );
+        System.out.println( "It is 25 July." );
     }
 }
